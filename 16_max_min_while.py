fdbe10300bed14764ea7e22b0d0d969a8861825d
Teo6507 <s-fare.matteo@isiskeynes.it>
2022-10-23 14:44:28 +0200

    [ADD] creatr il sedicesimo programma

---
+++ 16_max_min_while.py
@@ -0,0 +1,16 @@
+somma=0
+max=float('-inf')
+min=float('inf')
+while True:
+    numero=int(input("inserisci il numero: "))
+    if (numero==0):
+        break
+    else:
+        if (numero>max):
+            max=numero
+        if (numero<min):
+            min=numero
+print("Numero Massimo: "+ str(max))
+print("Numero Minimo: "+ str(min))
+
+a=input("press any key to exit")
