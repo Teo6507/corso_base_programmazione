cb495716ec2eef8d59629340a17293c7e5218da6
Teo6507 <s-fare.matteo@isiskeynes.it>
2022-11-02 20:47:03 +0100

    [ADD] Creato il diciassettesimo esercizio

--- 17_while_calcolatrice.py
+++ 17_while_calcolatrice.py
@@ -1,23 +1,27 @@
-numero_a=int(input('inserisci un numero: '))
-numero_b=int(input('inserisci un numero: '))
-segno=int(input('inserisci 1 per la somma, 2 per la sottrazione, 3 per la divisione o 4 per la moltiplicazione: '))
-output=0
-if(segno==1):
-    output=numero_a+numero_b
-else:
-    if(segno==2):
-        output=numero_a-numero_b
+operazioni=int(input("inserisci il numero di operazioni: "))
+output=int(input("inserisci il primo numero"))
+i=1
+
+while i<operazioni:
+    segno=int(input('inserisci 1 per la somma, 2 per la sottrazione, 3 per la divisione o 4 per la moltiplicazione: '))
+    numero_a=int(input("Inserisci un numero: "))
+    if(segno==1):
+        output=output+numero_a
     else:
-        if(segno==3):
-            if(numero_b==0):
-                output="Non puoi dividere per zero!"
-            else:
-                output=numero_a/numero_b
+        if(segno==2):
+            output=output-numero_a
         else:
-            if(segno==4):
-                output=numero_a*numero_b
+            if(segno==3):
+                if(numero_a==0):
+                    output="Non puoi dividere per zero!"
+                else:
+                    output=output/numero_a
             else:
-                output=numero_a+numero_b
+                if(segno==4):
+                    output=output*numero_a
+                else:
+                    output=output+numero_a
+    i=i+1
 print(output)
 
 exit=input("press any key to exit")
