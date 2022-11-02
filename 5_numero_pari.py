a4336c2fec8f4648652b5b7425cc7913bbf79a64
Teo6507 <s-fare.matteo@isiskeynes.it>
2022-11-02 20:45:09 +0100

    [ADD] Creato il quinto esercizio

---
+++ 5_numero_pari.py
@@ -0,0 +1,7 @@
+numero=int(input("Inserisci un numero pari: "))
+output="hai inserito un numero dispari birbante!"
+if numero % 2 == 0:
+    output=numero+3
+print(output)
+
+exit=input("press any key to exit")
