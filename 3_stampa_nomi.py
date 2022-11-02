682039d9e34c6711e18f116802b89b064e722c78
Teo6507 <s-fare.matteo@isiskeynes.it>
2022-11-02 20:44:43 +0100

    [ADD] Creato il terzo esercizio

---
+++ 3_stampa_nomi.py
@@ -0,0 +1,15 @@
+lettera=input("inserisci l'iniziale del nome: ")
+output="you choose... poorly"
+if lettera == "a":
+    output="Alina"
+if lettera == "b":
+    output="Bruno"
+if lettera == "c":
+    output="Carlo"
+if lettera == "d":
+    output="Dario"
+if lettera == "e":
+    output="Emilio"
+print(output)
+
+exit=input("press any key to exit")
